0431f713e978ac5a54c6d761f88595494e81e770
Adriano Junior <88254828+adrianodev97@users.noreply.github.com>
2023-01-17 10:16:30 -0300

    Feature libraries in a Nx

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -3,51 +3,67 @@ import React from 'react';
 import './app.scss';
 import { getAllGames } from '../fake-api';
 
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
+import { Header } from '@nxreactapp/store/ui-shared';
 
-import { Header } from "@nxreactapp/store/ui-shared";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
-import { formatRating } from "@nxreactapp/store/util-formatters";
+import { formatRating } from '@nxreactapp/store/util-formatters';
+
+import { Route, useHistory } from 'react-router-dom';
+
+import { StoreFeatureGameDetail } from '@nxreactapp/store/feature-game-detail';
 
 export const App = () => {
+  const history = useHistory();
+
   return (
     <>
-      <Header/>
+      <Header />
       <div className="container">
-      <div className="games-layout">
-        {getAllGames().map((x) => (
-          <Card key={x.id} className="game-card">
-            <CardActionArea>
-              <CardMedia
-                className="game-card-media"
-                image={x.image}
-                title={x.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {x.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {x.description}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  className="game-rating"
-                >
-                  <strong>Rating:</strong> {formatRating(x.rating)}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+        <div className="games-layout">
+          {getAllGames().map((x) => (
+            <Card
+              key={x.id}
+              className="game-card"
+              onClick={() => history.push(`/game/${x.id}`)}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className="game-card-media"
+                  image={x.image}
+                  title={x.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {x.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {x.description}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    className="game-rating"
+                  >
+                    <strong>Rating:</strong> {formatRating(x.rating)}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
+
+      <Route path="/game/:id" component={StoreFeatureGameDetail} />
     </>
   );
 };
